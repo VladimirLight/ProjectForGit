27de27a82e77b07a60090bdab11a8a550dbf0149
VladimirLight <SokolovVova1986@gmail.com>
2023-08-08 15:49:32 +0700

    corrected class Cats

--- Cats.java
+++ Cats.java
@@ -1,7 +1,10 @@
 package org.example;
 
-public class Cats extends Dogs{
-   Cats(){}
+public class Cats {
+   String Bread;
+   int Age;
+   double Weight;
+    Cats(){}
 
    public void Miau(){
        System.out.println("Miiaauu");
