3a38c7a49e242a46ac207e3775100311e1703219
VladimirLight <SokolovVova1986@gmail.com>
2023-08-24 15:33:09 +0700

    added again class Dogs

---
+++ Dogs.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Dogs extends Cats{
+
+
+    Dogs (String Bread_, int Age_, double Weight_){
+        Bread = Bread_;
+        Age = Age_;
+        Weight = Weight_;
+    }
+    Dogs (String Bread_){
+        Bread = Bread_;
+    }
+
+    public void Gav(){
+        System.out.println("Gav!");
+    }
+
+}
